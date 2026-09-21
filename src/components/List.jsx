de26acd8b435abfbbad64522423cf2c05bcd7d88
Felix <felixeklund74@gmail.com>
2026-09-21 20:27:15 +0200

    Added image display and upload button to List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,23 @@
-function List({ series, onToggleSeen, onDelete }) {
+function List({ series, onToggleSeen, onDelete, onImageUpload }) {
   return (
     <div className="list">
       {series.map((serie) => (
         <div key={serie.id} className="serie">
           <h2>{serie.title}</h2>
+          {serie.imageUrl && (
+  <img src={`http://localhost:5172${serie.imageUrl}`} alt={serie.title} width={120} />
+)}
+
+<label style={{ display: "block" }}></label>
+   <label style={{ cursor: "pointer" }}>
+  📷 {serie.imageUrl ? "Byt bild" : "Ladda upp"}
+  <input
+    type="file"
+    accept="image/jpeg,image/png"
+    style={{ display: "none" }}
+    onChange={(e) => e.target.files[0] && onImageUpload(serie.id, e.target.files[0])}
+  />
+</label>
           <div className="serie-row">
             <p>Säsong {serie.season}, avsnitt {serie.episode}</p>
             <label>
